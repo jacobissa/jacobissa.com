ce02d9402d90f4e2154ecba5ec4dc799f829dafe
Jacob Issa <jacobisssa@gmail.com>
2022-08-31 22:31:35 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <title>Jacob Issa</title>
+
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="title" content="Jacob Issa">
+</head>
+
+<body>
+    
+</body>
+
+</html>
