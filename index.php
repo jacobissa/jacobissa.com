f1fab83319e5f5c9e7e3151540c9f93089cc7d1b
Jacob Issa <jacobisssa@gmail.com>
2022-08-31 23:10:22 +0200

    include bootstrap lib

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php ?>
+<?php
+$bootstrap_version = "5.2.0";
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -8,10 +10,17 @@
     <!-- Meta Tags -->
     <meta charset="UTF-8">
     <meta name="title" content="Jacob Issa">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="./lib/bootstrap/css/bootstrap.min.css?v=<?php echo $bootstrap_version; ?>">
 </head>
 
 <body>
-    
+    <h1>Jacob Issa</h1>
+
+    <!-- JS -->
+    <script src="./lib/bootstrap/js/bootstrap.bundle.min.js?v=<?php echo $bootstrap_version; ?>"></script>
 </body>
 
 </html>
